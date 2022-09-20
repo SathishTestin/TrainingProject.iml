a133bfab59de8a844e961c82636f397ac448a435
SathishTestin <107636304+SathishTestin@users.noreply.github.com>
2022-09-20 11:34:19 +0530

    Update ForloopConept.java

--- src/MyTask/ForloopConept.java
+++ src/MyTask/ForloopConept.java
@@ -1,9 +1,14 @@
 package MyTask;
 
 public class ForloopConept {
-
+// super power of class:
+// class contains variables ,method, mainmethod,object
+// one class to another class we using extends keyword
+//we declare the class as a final class(){} we can't inherit
     public static void main(String[] args) {
-
+//// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
 //        for(int i=0; i<3; i++){
 //
 //            for(int j =0; j<4; j++){
@@ -14,18 +19,24 @@ public class ForloopConept {
 //
 //        }
 
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 5; i++) {//// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
 
            // System.out.print(" ");
 
-            for (int j=0; j<=i; j++){
+            for (int j=0; j<=i; j++){//// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
 
                 System.out.print("#");
             }
             System.out.println();
         }
 
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 3; i++) {//// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
@@ -34,7 +45,9 @@ public class ForloopConept {
 
         }
 
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 3; i++) {//// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
             for (int j = i + 1; j <= 3; j++) {
                 System.out.println(j);
             }
