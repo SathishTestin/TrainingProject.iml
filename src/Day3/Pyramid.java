a0cac5b8a9dd7e61b7be044426f1c0979c34962c
SathishTestin <107636304+SathishTestin@users.noreply.github.com>
2022-09-20 11:32:22 +0530

    Update Pyramid.java

--- src/Day3/Pyramid.java
+++ src/Day3/Pyramid.java
@@ -1,7 +1,11 @@
 package Day3;
 
 public class Pyramid {
-
+// super power of class:
+// class contains variables ,method, mainmethod,object
+// one class to another class we using extends keyword
+//we declare the class as a final class(){} we can't inherit
+    
     public static void main(String[] args) {
 
         int row=5; //initialization declaration
@@ -9,12 +13,21 @@ public class Pyramid {
 
         for (int i=1; i<=row;i++){ //for loop
 
+// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
             // for spaces
-            for (int j=4;j>=i; j--){ //for loop
+            for (int j=4;j>=i; j--){ //
+// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
                 System.out.print(" ");
             }
 
-            for (int k=1; k<=num; k++){ //for loop
+            for (int k=1; k<=num; k++){ //
+// For loop is repeating action
+//for loop travel both forward and backward direction
+// for is condition based one
                 System.out.print("*");
             }
             System.out.println();
