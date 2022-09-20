7cd73674f2b3d1d3ec53ba79465a287e6739fe41
Administrator <m.sathishcivil97@gmail.com>
2022-09-20 10:25:25 +0530

    I add

---
+++ src/Day3/NumberSeries.java
@@ -0,0 +1,18 @@
+package Day3;
+
+public class NumberSeries {
+    static int a = 7;
+
+    public static void main(String[] args) {
+
+        if (a % 2 == 0) {
+
+            System.out.println("This is even number");
+        } else {
+            System.out.println("This is odd Number");
+
+        }
+
+
+    }
+}
